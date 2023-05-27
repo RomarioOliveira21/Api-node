abfceda5e0f3ffb966db8b2f8d63ef7b330ea811
Romario de Oliveira <87107730+RomarioOliveira21@users.noreply.github.com>
2023-05-27 14:06:59 -0300

    Criando projeto

---
+++ src/models/personModel.js
@@ -0,0 +1,242 @@
+exports.Persons = [
+    {
+        "nome": "João",
+        "idade": 25,
+        "id": 1,
+        "cor": "azul",
+        "cidade": "São Paulo",
+        "uf": "SP"
+    },
+    {
+        "nome": "Maria",
+        "idade": 30,
+        "id": 2,
+        "cor": "vermelho",
+        "cidade": "Rio de Janeiro",
+        "uf": "RJ"
+    },
+    {
+        "nome": "Pedro",
+        "idade": 40,
+        "id": 3,
+        "cor": "verde",
+        "cidade": "Belo Horizonte",
+        "uf": "MG"
+    },
+    {
+        "nome": "Ana",
+        "idade": 22,
+        "id": 4,
+        "cor": "amarelo",
+        "cidade": "Salvador",
+        "uf": "BA"
+    },
+    {
+        "nome": "Carlos",
+        "idade": 35,
+        "id": 5,
+        "cor": "roxo",
+        "cidade": "Brasília",
+        "uf": "DF"
+    },
+    {
+        "nome": "Sandra",
+        "idade": 28,
+        "id": 6,
+        "cor": "laranja",
+        "cidade": "Recife",
+        "uf": "PE"
+    },
+    {
+        "nome": "Fernando",
+        "idade": 33,
+        "id": 7,
+        "cor": "rosa",
+        "cidade": "Fortaleza",
+        "uf": "CE"
+    },
+    {
+        "nome": "Mariana",
+        "idade": 29,
+        "id": 8,
+        "cor": "cinza",
+        "cidade": "Manaus",
+        "uf": "AM"
+    },
+    {
+        "nome": "Lucas",
+        "idade": 27,
+        "id": 9,
+        "cor": "marrom",
+        "cidade": "Curitiba",
+        "uf": "PR"
+    },
+    {
+        "nome": "Isabela",
+        "idade": 31,
+        "id": 10,
+        "cor": "preto",
+        "cidade": "Porto Alegre",
+        "uf": "RS"
+    },
+    {
+        "nome": "Rafael",
+        "idade": 26,
+        "id": 11,
+        "cor": "branco",
+        "cidade": "Goiânia",
+        "uf": "GO"
+    },
+    {
+        "nome": "Camila",
+        "idade": 32,
+        "id": 12,
+        "cor": "violeta",
+        "cidade": "Natal",
+        "uf": "RN"
+    },
+    {
+        "nome": "Gustavo",
+        "idade": 23,
+        "id": 13,
+        "cor": "verde limão",
+        "cidade": "Florianópolis",
+        "uf": "SC"
+    },
+    {
+        "nome": "Letícia",
+        "idade": 24,
+        "id": 14,
+        "cor": "azul turquesa",
+        "cidade": "Vitória",
+        "uf": "ES"
+    },
+    {
+        "nome": "Ricardo",
+        "idade": 37,
+        "id": 15,
+        "cor": "laranja",
+        "cidade": "Cuiabá",
+        "uf": "MT"
+    },
+    {
+        "nome": "Juliana",
+        "idade": 36,
+        "id": 16,
+        "cor": "rosa",
+        "cidade": "Campo Grande",
+        "uf": "MS"
+    },
+    {
+        "nome": "Daniel",
+        "idade": 34,
+        "id": 17,
+        "cor": "cinza",
+        "cidade": "Teresina",
+        "uf": "PI"
+    },
+    {
+        "nome": "Patrícia",
+        "idade": 39,
+        "id": 18,
+        "cor": "amarelo",
+        "cidade": "João Pessoa",
+        "uf": "PB"
+    },
+    {
+        "nome": "Hugo",
+        "idade": 38,
+        "id": 19,
+        "cor": "verde",
+        "cidade": "Aracaju",
+        "uf": "SE"
+    },
+    {
+        "nome": "Carolina",
+        "idade": 21,
+        "id": 20,
+        "cor": "roxo",
+        "cidade": "Belém",
+        "uf": "PA"
+    },
+    {
+        "nome": "Eduardo",
+        "idade": 43,
+        "id": 21,
+        "cor": "vermelho",
+        "cidade": "Maceió",
+        "uf": "AL"
+    },
+    {
+        "nome": "Larissa",
+        "idade": 42,
+        "id": 22,
+        "cor": "azul",
+        "cidade": "Boa Vista",
+        "uf": "RR"
+    },
+    {
+        "nome": "Roberto",
+        "idade": 45,
+        "id": 23,
+        "cor": "verde limão",
+        "cidade": "Macapá",
+        "uf": "AP"
+    },
+    {
+        "nome": "Amanda",
+        "idade": 44,
+        "id": 24,
+        "cor": "azul turquesa",
+        "cidade": "Palmas",
+        "uf": "TO"
+    },
+    {
+        "nome": "André",
+        "idade": 47,
+        "id": 25,
+        "cor": "preto",
+        "cidade": "Porto Velho",
+        "uf": "RO"
+    },
+    {
+        "nome": "Fernanda",
+        "idade": 46,
+        "id": 26,
+        "cor": "branco",
+        "cidade": "Rio Branco",
+        "uf": "AC"
+    },
+    {
+        "nome": "Rafaela",
+        "idade": 49,
+        "id": 27,
+        "cor": "marrom",
+        "cidade": "São Luís",
+        "uf": "MA"
+    },
+    {
+        "nome": "Paulo",
+        "idade": 48,
+        "id": 28,
+        "cor": "violeta",
+        "cidade": "Curitiba",
+        "uf": "PR"
+    },
+    {
+        "nome": "Luana",
+        "idade": 50,
+        "id": 29,
+        "cor": "vermelho",
+        "cidade": "Brasília",
+        "uf": "DF"
+    },
+    {
+        "nome": "Marcelo",
+        "idade": 41,
+        "id": 30,
+        "cor": "roxo",
+        "cidade": "São Paulo",
+        "uf": "SP"
+    }
+];
